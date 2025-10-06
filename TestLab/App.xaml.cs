4d4dc56b46919eec9b746cc321d739fda4f5061c
Kiet Truong <jadentruong2003@Mac.lan>
2025-10-06 15:14:22 +0700

    first commit

---
+++ TestLab/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestLab;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ TestLab/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TestLab;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
